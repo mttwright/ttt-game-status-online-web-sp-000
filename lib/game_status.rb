99d866f2e75fc9ea20538b37dbb0787d81c0f218
Matt Wright <mttwright@gmail.com>
2020-08-15 00:23:30 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,4 +5,11 @@ end
 
 # Define your WIN_COMBINATIONS constant
 
-WIN_COMBINATIONS = ["","","","","","","",""]
+WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [2, 1, 0],
+    [0, 3, 6],
+    [6, 3, 0],
+    [3, 4, 5],
+    [2, 4, 6]
+  ]
