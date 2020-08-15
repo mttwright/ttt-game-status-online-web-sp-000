a24c26776eb4e4fac9e07e26916723850eec67d7
Matt Wright <mttwright@gmail.com>
2020-08-15 00:53:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,11 +28,11 @@ def won?(board)
     pos_3 = board[win_index_3]
     
     if pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
-      answer = true
+      return win_combo
     end
     
     if pos_1 == "O" && pos_2 == "O" && pos_3 == "O"
-      answer = true
+      return win_combo
     end
   end
   answer
